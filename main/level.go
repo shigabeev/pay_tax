18d0c689978f3661d77a084e93a9c0219395a366
Ilya Shigabeev <frappuccino@Kraken.local>
2019-09-28 17:30:25 +0300

    initial commit

---
+++ main/level.go
@@ -0,0 +1,48 @@
+components {
+  id: "level_1"
+  component: "/main/level.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "level"
+  component: "/main/level.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "brickfactory"
+  type: "collectionfactory"
+  data: "prototype: \"/main/brick/brick.collection\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
