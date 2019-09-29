f39ed5d4d5d1bf3c2728cc2e5824e1d1ebfc2abd
Ilya Shigabeev <frappuccino@Kraken.local>
2019-09-29 05:21:25 +0300

    added fonts and controls

--- main/taxi/stars/factory.go
+++ main/taxi/stars/factory.go
@@ -1,6 +1,6 @@
 components {
   id: "script"
-  component: "/stars/factory.script"
+  component: "/main/taxi/stars/factory.script"
   position {
     x: 0.0
     y: 0.0
@@ -16,7 +16,7 @@ components {
 embedded_components {
   id: "star_factory"
   type: "factory"
-  data: "prototype: \"/stars/star.go\"\n"
+  data: "prototype: \"/main/taxi/stars/star.go\"\n"
   "load_dynamically: false\n"
   ""
   position {
