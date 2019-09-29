0996e5fcfea69dc82d143e8e3fc78403607ebd56
Ilya Shigabeev <frappuccino@Kraken.local>
2019-09-29 07:02:51 +0300

    taxi is ok

--- main/taxi/background/large_planets.go
+++ main/taxi/background/large_planets.go
@@ -37,7 +37,7 @@ embedded_components {
   id: "sprite1"
   type: "sprite"
   data: "tile_set: \"/main/taxi/background/background.atlas\"\n"
-  "default_animation: \"gas_planet\"\n"
+  "default_animation: \"road\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
